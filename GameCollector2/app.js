ae278488f92391ef0e516db0e0ed50e8bba2dc3f
Kenny Varghese <varghese.kenny@gmail.com>
2019-04-24 06:28:26 -0400

    Created pages
    Registration, Login, and added URL for homepage

--- GameCollector2/app.js
+++ GameCollector2/app.js
@@ -1,67 +1,3 @@
-/*var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;*/
-
-
-
-//old app.js code
-//code copied from nodejs.org/about to run the server
-/*var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Kenny can go suck a dick!!!!!\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(Server running at http://${hostname}:${port}/);
-});*/
-
 const express = require('express');
 const bodyParser = require('body-parser')
 const app = express();
@@ -80,12 +16,25 @@ app.use(bodyParser.json());
 
 
 app.get('/', function(req, res) {
-  res.render('homePage');
-  //res.send('Kenny can go suck a dick!!!!!\n');
+  res.render('loginPage');
+
 });
+
+app.get('/registrationPage', function(req, res){
+      res.render('registrationPage');
+})
+
+app.get('/homePage', function(req, res){
+  res.render('homePage');
+});
+
+app.get('/loginPage', function(req, res){
+  res.render('loginPage');
+});
+
 app.get('/index', function(req, res) {
   res.render('index');
-  //res.send('Kenny can go suck a dick!!!!!\n');
+
 });
 app.get('/snake', function(req, res) {
   res.render('snake');
@@ -94,8 +43,13 @@ app.get('/snake', function(req, res) {
 app.get('/connect4', function(req, res) {
   res.render('connect4');
 });
+
 app.get('/ticTacToe', function(req, res){
   res.render('ticTacToe');
+});
+
+app.get('/highscore', function(req, res){
+      res.render('highscore');
 });
 
 app.listen(port, function(){
